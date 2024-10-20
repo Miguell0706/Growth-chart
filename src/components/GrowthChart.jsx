175e8c808d21b6ebe0a7338eb2032b526791464f
Miguel <Miguellozano3757@gmail.com>
2024-10-19 21:39:16 -0700

    made ruler and giraffe responsive

--- src/components/GrowthChart.jsx
+++ src/components/GrowthChart.jsx
@@ -2,17 +2,57 @@ import "../styles/GrowthChart.css";
 // src/GrowthChart.jsx
 import React from "react";
 const giraffeImage = "/images/giraffe2.png";
+const measurements = Array.from({ length: 16 }, (_, i) => (18 - i) * 10);
 
 const GrowthChart = () => {
   return (
     <div className="chart-container">
-      {/* Measurement Lines and Profile Pictures */}
+      {/* Giraffe Image */}
       <img src={giraffeImage} alt="Giraffe" className="giraffe" />
+
+      {/* Measurement Lines and Profile Pictures */}
       <div className="measurements">
-        {/* Measurement Ticks and Labels */}
-        {Array.from({ length: 18 }, (_, i) => (
-          <div key={i} className="tick">
-            <span>{(i + 1) * 10}</span>
+        {measurements.map((value) => (
+          <div className="measurement-quantity" key={value}>
+            <span>{value}</span>
+            <div className="ticks">
+              {value === 180 ? (
+                <div className="big-tick-180">_____</div>
+              ) : (
+                <>
+                  <div className="small-tick" key={`${value}-1`}>
+                    __
+                  </div>
+                  <div className="small-tick" key={`${value}-2`}>
+                    __
+                  </div>
+                  <div className="small-tick" key={`${value}-3`}>
+                    __
+                  </div>
+                  <div className="small-tick" key={`${value}-4`}>
+                    __
+                  </div>
+                  <div className="big-tick" key={`${value}-5`}>
+                    ____
+                  </div>
+                  <div className="small-tick" key={`${value}-6`}>
+                    __
+                  </div>
+                  <div className="small-tick" key={`${value}-7`}>
+                    __
+                  </div>
+                  <div className="small-tick" key={`${value}-8`}>
+                    __
+                  </div>
+                  <div className="small-tick" key={`${value}-9`}>
+                    __
+                  </div>
+                  <div className="big-tick" key={`${value}-10`}>
+                    ______
+                  </div>
+                </>
+              )}
+            </div>
           </div>
         ))}
       </div>
